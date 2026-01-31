d2dd6f4ca9d85117ea717bebc74870270959d5c6
seifallahabirga <seifallahabiriga09@gmail.com>
2026-01-31 23:14:22 +0100

    hhhhhhhh mala winek enti

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:8000",
+        "http://localhost:8080",
+    ],
+    allow_credentials=True,
+
     allow_methods=["*"],
     allow_headers=["*"]
 )
